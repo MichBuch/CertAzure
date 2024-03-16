5736f9b22c3581f2f6d0f73b1b7e1ddaf3a1b311
MichBuch <Michael.Buchanan@btinternet.com>
2024-03-16 18:10:38 +0000

    160324

--- obj/Debug/net8.0/CertAzure.AssemblyInfo.cs
+++ obj/Debug/net8.0/CertAzure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertAzure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e45f4ae7c4c9f09b6732416826ad4ba2b6b7c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5652f8cc9afbcfa93be72604b48345f39eccc62d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertAzure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertAzure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
